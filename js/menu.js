bcd4e910a3eb953c56cd706706390d7531638312
Volodymyr-Raskazov <wladras77@gmail.com>
2023-09-11 14:46:29 +0300

    some minor fixes

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
 const menu = document.getElementById('menu');
 const menuBtn = document.getElementById('menu-btn');
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', () => {
 	toggleActiveClass(menu);
 	toggleActiveClass(menuBtn);
+	toggleActiveClass(body);
 });
 
 function toggleActiveClass(element) {
